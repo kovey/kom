b2523617fe97231ff2831110943748da0230d197
kovey <kovey@vip.qq.com>
2023-11-10 10:54:14 +0800

    use new context

--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/kovey/cli-go v1.0.8
 	github.com/kovey/debug-go v0.0.4
 	github.com/kovey/discovery v0.0.1
-	github.com/kovey/pool v0.0.1
+	github.com/kovey/pool v0.0.3
 	go.uber.org/zap v1.25.0
 	google.golang.org/grpc v1.58.0
 	google.golang.org/protobuf v1.31.0
--- service/conainer.go
+++ service/conainer.go
@@ -8,25 +8,9 @@ import (
 )
 
 func container(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
-	nCtx := pool.Get[*Context](ctx_namespace, ctx_full_name, ctx)
-	defer put(nCtx)
-
-	if descs, ok := svs.objs[info.FullMethod]; ok {
-		for _, desc := range descs {
-			if obj := pool.GetObject(desc.Namespace, desc.Name, ctx); obj != nil {
-				nCtx.add(obj)
-			}
-		}
-	}
+	nCtx := pool.NewContext(ctx)
+	defer pool.PutNoCtx(nCtx)
 
 	resp, err := handler(nCtx, req)
 	return resp, err
-}
-
-func put(ctx *Context) {
-	for _, obj := range ctx.depObjs {
-		pool.Put(obj)
-	}
-
-	pool.Put(ctx)
 }
