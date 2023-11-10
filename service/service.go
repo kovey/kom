ac9f3dfab76ade751b47fe516fe682ed8a7a193f
kovey <kovey@vip.qq.com>
2023-11-10 10:57:24 +0800

    use new context

--- service/service.go
+++ service/service.go
@@ -8,17 +8,6 @@ import (
 
 type ServiceInterface interface {
 	Desc() *grpc.ServiceDesc
-	Deps() map[string][]*ObjDesc
-}
-
-type ObjDesc struct {
-	Namespace string
-	Name      string
-	Method    string
-}
-
-func NewObjDesc(namespace, name, method string) *ObjDesc {
-	return &ObjDesc{Namespace: namespace, Name: name, Method: method}
 }
 
 type method struct {
@@ -28,7 +17,6 @@ type method struct {
 
 type Base struct {
 	desc *grpc.ServiceDesc
-	deps map[string][]*ObjDesc
 }
 
 func getFullMethod(desc *grpc.ServiceDesc) []*method {
@@ -40,46 +28,24 @@ func getFullMethod(desc *grpc.ServiceDesc) []*method {
 	return res
 }
 
-func NewBase(desc *grpc.ServiceDesc, objs ...*ObjDesc) *Base {
-	b := &Base{desc: desc, deps: make(map[string][]*ObjDesc)}
-	for _, method := range getFullMethod(desc) {
-		for _, obj := range objs {
-			if obj.Method == "" {
-				b.deps[method.fullName] = append(b.deps[method.fullName], obj)
-				continue
-			}
-
-			if obj.Method == method.name {
-				b.deps[method.fullName] = append(b.deps[method.fullName], obj)
-			}
-		}
-	}
-
-	return b
+func NewBase(desc *grpc.ServiceDesc) *Base {
+	return &Base{desc: desc}
 }
 
 func (b *Base) Desc() *grpc.ServiceDesc {
 	return b.desc
 }
 
-func (b *Base) Deps() map[string][]*ObjDesc {
-	return b.deps
-}
-
 type services struct {
-	svs  []ServiceInterface
-	objs map[string][]*ObjDesc
+	svs []ServiceInterface
 }
 
 func newServices() *services {
-	return &services{objs: make(map[string][]*ObjDesc)}
+	return &services{}
 }
 
 func (s *services) add(sv ServiceInterface) {
 	s.svs = append(s.svs, sv)
-	for method, objs := range sv.Deps() {
-		s.objs[method] = objs
-	}
 }
 
 var svs = newServices()
