401f52e7689e0ab0b21ef9c7121567ad393c6cae
kovey <kovey@vip.qq.com>
2024-11-08 21:42:33 +0800

    remove config, use .env

--- server/run.go
+++ server/run.go
@@ -1,13 +1,16 @@
 package server
 
 import (
+	"os"
+
 	"github.com/kovey/cli-go/app"
+	"github.com/kovey/cli-go/env"
 	"github.com/kovey/debug-go/debug"
 )
 
-func Run(e EventInterface, name string) {
+func Run(e EventInterface) {
 	serv := newServer(e)
-	cli := app.NewApp(name)
+	cli := app.NewApp(os.Getenv(env.APP_NAME))
 	cli.SetServ(serv)
 	if err := cli.Run(); err != nil {
 		debug.Erro(err.Error())
