8d0dadb3e2bf7a62d8e97295590599c8b2179371
kovey <kovey@vip.qq.com>
2025-05-26 14:24:37 +0800

    add debug config

--- server/run.go
+++ server/run.go
@@ -1,7 +1,10 @@
 package server
 
 import (
+	"os"
+
 	"github.com/kovey/cli-go/app"
+	"github.com/kovey/cli-go/env"
 	"github.com/kovey/debug-go/debug"
 )
 
@@ -12,6 +15,10 @@ func Run(e EventInterface) {
 		appName = e.AppName()
 	}
 	cli := app.NewApp(appName)
+	cli.SetDebugLevel(debug.DebugType(os.Getenv(env.DEBUG_LEVEL)))
+	if line, err := env.GetInt(env.DEBUG_SHOW_FILE); err == nil {
+		debug.SetFileLine(debug.FileLine(line))
+	}
 	cli.SetServ(serv)
 	if err := cli.Run(); err != nil {
 		debug.Erro(err.Error())
